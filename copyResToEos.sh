6011d984b6c26608a6723161de1722a37c2348e7
noepalm <palmeri.1871005@studenti.uniroma1.it>
2023-05-10 11:58:14 +0200

    Removed unused .sh files

--- copyResToEos.sh
+++
@@ -1,6 +0,0 @@
-rm -r $MYEOS/TnP/nominalFit
-echo "removed old plots"
-cp -r results/muons/DoubleMu/plots/data/nominalFit/ $MYEOS/TnP
-cp results/muons/DoubleMu/egammaEffi.txt_egammaPlots.pdf $MYEOS/TnP
-cp results/muons/DoubleMu/egammaEffi.txt $MYEOS/TnP
-echo "copied new plots + summary plot"
--- runAndCopy.sh
+++
@@ -1,3 +0,0 @@
-python tnpEGM_fitter.py etc/config/settings.py --flag DoubleMu --doFit
-python tnpEGM_fitter.py etc/config/settings.py --flag DoubleMu --sumUp
-. copyResToEos.sh
